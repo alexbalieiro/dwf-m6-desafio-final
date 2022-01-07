0be0b33d4d9f1a01c98eefbbed1164123b724078
Alex Bal <alexenri12@gmail.com>
2022-01-07 19:05:42 -0300

    add env vars

--- server/db.ts
+++ server/db.ts
@@ -4,7 +4,7 @@ require("dotenv").config();
 const service = JSON.parse(process.env.FIREBASE_CONFIG);
 admin.initializeApp({
   credential: admin.credential.cert(service),
-  databaseURL: "https://apx-dwf-m6-a0ea8-default-rtdb.firebaseio.com",
+  databaseURL: process.env.FIRABESE_URL,
 });
 
 const firestore = admin.firestore();
