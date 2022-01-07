8d18351ec554c3bed6292adbd3f41c0727156b73
Alex Bal <alexenri12@gmail.com>
2022-01-07 17:58:35 -0300

    env vars

--- server/index.ts
+++ server/index.ts
@@ -1,18 +1,48 @@
 import * as express from "express";
+import * as path from "path";
+import { firestore, rtdb } from "./db";
+import * as cors from "cors";
 (function () {
   const port = process.env.PORT || 3000;
   const app = express();
   app.use(express.json());
+  app.use(cors());
 
-  app.get("/hola", (req, res) => {
-    res.json({
-      message: "hola soy el servidor",
-    });
+  console.log(process.env.FIREBASE_PROJECT_ID);
+
+  const userCollection = firestore.collection("users");
+
+  app.post("/signup", function (req, res) {
+    const email = req.body.email;
+    const nombre = req.body.nombre;
+    userCollection
+      .where("email", "==", email)
+      .get()
+      .then((searchResponse) => {
+        if (searchResponse.empty) {
+          userCollection
+            .add({
+              email: email,
+              nombre,
+            })
+            .then((newUserRef) => {
+              res.json({
+                id: newUserRef.id,
+                new: true,
+              });
+            });
+        } else {
+          res.status(400).json({
+            message: "user already exists",
+          });
+        }
+      });
   });
   app.use(express.static("dist"));
 
   app.get("*", (req, res) => {
-    res.sendFile(__dirname + "/dist/index.html");
+    const rutaRelativa = path.resolve(__dirname, "../dist/", "index.html");
+    res.sendFile(rutaRelativa);
   });
   app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
